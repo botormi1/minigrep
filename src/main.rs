e56af9aa58e9ad81fa63e0ac0269d51ff6f4c1f5
botormi1 <botormi1@gmail.com>
2023-01-06 23:59:09 +0100

    adding function 'run' reading the file

--- src/main.rs
+++ src/main.rs
@@ -15,6 +15,10 @@ fn main() {
         config.query, config.filename
     );
 
+    run(config);
+}
+
+fn run(config: Config) {
     let contents =
         fs::read_to_string(config.filename).expect("Something went wrong reading a file");
 
